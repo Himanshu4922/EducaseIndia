10880cfe91c3906aaa58bceecefabadc341e76b1
himanshu <bishthimanshu829@gmail.com>
2025-04-29 17:31:16 +0530

    Initial commit for Educase India Assignment

---
+++ src/pages/Register.jsx
@@ -0,0 +1,13 @@
+import InputContainer from "./../components/InputContainer";
+import RegisterForm from "./../components/RegisterForm";
+
+function Register() {
+
+  return (
+    <>
+      <RegisterForm/>
+    </>
+  );
+}
+
+export default Register;
