9d8c46b4e1f6b7a3955a130c3db2a0ffbc167a3e
himanshu <bishthimanshu829@gmail.com>
2025-04-30 10:39:54 +0530

    Remove unnecessary appwrite config file

--- src/appwriteConfig.js
+++
@@ -1,9 +0,0 @@
-import { Client, Account } from "appwrite";
-
-const client = new Client();
-client
-  .setEndpoint("https://fra.cloud.appwrite.io/v1")
-  .setProject("680f5b090004e41bb7e5");
-
-export const account = new Account(client);
-export default client;
